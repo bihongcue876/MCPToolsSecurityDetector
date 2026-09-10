1a58f23d884e07d9c2aa2691f02a1d5f7f78a6b9
bihongcue876 <1581672842@qq.com>
2026-09-10 19:24:01 +0800

    init: 添加record文件与文件目录

--- src/app_config.py
+++ src/app_config.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import json
+from datetime import datetime
 # 文件路径、信息管理、默认参数
 APP_NAME = "mcp-security-detector"
 VERSION = "0.0.0"
@@ -12,6 +13,7 @@ SETTINGS_PATH = DATA_DIR / "settings.json"
 SERVERS_CFG_PATH = DATA_DIR / "servers.json"
 DEFAULT_LOGS_PATH = DATA_DIR / "logs.log"
 USER_PAYLOADS_PATH = DATA_DIR / "payloads.json" # 进攻荷载或自定义资源
+RECORD_DIR = DATA_DIR / "record"
 # resources
 RESOURCES_DIR = BASE_DIR / "src" / "resources"
 DEFAULT_PAYLOADS_PATH = RESOURCES_DIR / "default_payloads.json"
@@ -22,6 +24,7 @@ MAX_LOG_LINES = 100
 def ensure_directories():
     """当默认文件不存在时，创建默认空文件"""
     DATA_DIR.mkdir(parents=True,exist_ok=True)
+    RECORD_DIR.mkdir(parents=True,exist_ok=True)
     # setting.json default
     if not SETTINGS_PATH.exists():
         default_settings = {
@@ -38,4 +41,13 @@ def ensure_directories():
     # logs default
     if not DEFAULT_LOGS_PATH.exists():
         DEFAULT_LOGS_PATH.touch() 
-    
+        
+def ensure_record_files():
+    """记录文件检查"""
+    month = datetime.now().strftime("%Y_%m")
+    RECORD_DIR.mkdir(parents=True, exist_ok=True)
+    toolcall_path = RECORD_DIR / f"tool_call_{month}.log"
+    detect_path = RECORD_DIR / f"detection_{month}.log"
+    toolcall_path.touch(exist_ok=True)
+    detect_path.touch(exist_ok=True)
+    return toolcall_path, detect_path
